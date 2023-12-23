64a0381a4a3c026042bef3ded7d9a0cfd77a12a1
ShivamKandekar <kandekarshivam2001@gmail.com>
2023-12-23 13:53:12 +0530

    uploading created project

---
+++ src/socket.js
@@ -0,0 +1,10 @@
+import { io } from "socket.io-client";
+
+export const initSocket = async () => {
+  return io("http://localhost:1337", {
+    "force new connection": true,
+    reconnectionAttempt: "Infinity",
+    timeout: 10000,
+    transports: ["websocket"],
+  });
+};
